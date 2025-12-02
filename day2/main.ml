c0e80936f1e577aea2a18088e861c6f9a3008d4b
Thibault Martin <thibault.martin@cea.fr>
2025-12-02 21:01:05 +0100

    Perf & code improvement
    Use Fibers and Domain manager to run each range un parallel

--- day2/main.ml
+++ day2/main.ml
@@ -16,35 +16,40 @@ end
 
 module Solving = struct
 
+  let invalid_pattern s len size =
+    if len mod size <> 0 then false
+    else
+      let pattern = String.sub s 0 size in
+      List.init ((len / size) -1) Fun.id
+      |> List.for_all (fun i ->
+          String.sub s ((i + 1) * size) size |> String.equal pattern
+        )
+
   let is_invalid_p1 s =
     let len = String.length s in
-    if len = 0 then true
-    else if len mod 2 = 1 then false
-    else
-      let half = len / 2 in
-      let first_half = String.sub s 0 half in
-      let second_half = String.sub s half half in
-      String.equal first_half second_half
+    if len mod 2 = 1 then false
+    else invalid_pattern s len (len / 2)
 
   let is_invalid_p2 s =
     let len = String.length s in
-    let rec check_pattern size =
-      if size > len / 2 then false
-      else if len mod size <> 0 then check_pattern (size + 1)
-      else
-        let rep = len / size in
-        let pattern = String.sub s 0 size in
-        let expected_string = String.concat "" (List.init rep (fun _ -> pattern)) in
-        if String.equal s expected_string then true
-        else check_pattern (size + 1)
+    let rec aux size =
+      if size = 0 then false
+      else if invalid_pattern s len size then true
+      else aux (size - 1)
     in
-    check_pattern 1
+    aux (len / 2)
+
+  let part_aux ~domain_mgr ~f input =
+    let f acc i = if f (string_of_int i) then (acc + i) else acc in
+    Eio.Fiber.List.map (fun (min, max) ->
+        Eio.Domain_manager.run domain_mgr @@ fun () ->
+        Misc_utils.fold_range ~init:0 ~f ~min ~max
+      ) input
+    |> List.fold_left ( + ) 0
 
   let part_aux ~f input =
-    let f acc i = if f (string_of_int i) then (acc + i) else acc in
-    List.fold_left (fun acc (min, max) ->
-        Misc_utils.fold_range ~init:acc ~f ~min ~max
-      ) 0 input
+    Eio_main.run @@ fun env ->
+    part_aux ~domain_mgr:(Eio.Stdenv.domain_mgr env) ~f input
 
   let part1 (input : Types.input) : Types.output =
     part_aux ~f:is_invalid_p1 input
