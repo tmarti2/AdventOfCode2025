8ba2df567ae4aed3ada5a7c10b3b0a7ec137457f
Thibault Martin <thibault.martin@cea.fr>
2025-12-05 00:44:48 +0100

    Day1 part 2

--- day1/main.ml
+++ day1/main.ml
@@ -24,25 +24,38 @@ module Solving = struct
 
   let max = 100
 
-  let value = function
-    | L i -> -i
-    | R i -> i
-
-  let add dial v = (dial + v) % max
-
-  let count_0_end dial r cpt =
-    let res = add dial (value r) in
-    if res = 0 then res, cpt + 1 else res, cpt
-
   let init = 50, 0
 
-  let part1 (input : input) : output =
-    List.fold_left ~init ~f:(fun (dial, cpt) r ->
-        count_0_end dial r cpt
-      ) input
+  let count_0_end dial r cpt =
+    let v = match r with L i -> -i | R i -> i in
+    let res = (dial + v) % max in
+    if res = 0 then res, cpt + 1 else res, cpt
+
+  let count_0_pass dial rot cpt =
+    match rot with
+    | R i ->
+      let raw = dial + i in
+      raw % max, cpt + raw / max
+    | L i ->
+      let raw_dial = dial - i in
+      let new_dial = raw_dial % max in
+      let zeros =
+        if dial = 0 then i / max
+        else if dial = i then 1
+        else if dial < i then (max - dial + i) / max
+        else 0
+      in
+      (new_dial, cpt + zeros)
+
+  let part_aux ~f input =
+    List.fold_left ~init ~f:(fun (dial, cpt) r -> f dial r cpt) input
     |> snd
 
-  let part2 (_input : input) : output = 0
+  let part1 (input : input) : output =
+    part_aux ~f:count_0_end input
+
+  let part2 (input : input) : output =
+    part_aux ~f:count_0_pass input
 end
 
 module Today = MakeDay (Types) (Parsing) (Solving)
